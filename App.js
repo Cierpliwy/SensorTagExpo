19f83aab6e1520bd5e00fb99a9bd905c45fb9896
Przemysław Lenart <przemek.lenart@polidea.com>
2018-09-25 11:44:13 +0200

    Setup iOS

--- App.js
+++ App.js
@@ -1,21 +1,121 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  FlatList,
+  Platform,
+  Button
+} from "react-native";
+import { BleManager, Device, BleError, LogLevel } from "react-native-ble-plx";
 
-export default class App extends React.Component {
+type Props = {};
+
+type State = {
+  text: Array<string>
+};
+
+function arrayBufferToHex(buffer) {
+  if (!buffer) return null;
+  const values = new Uint8Array(buffer);
+  var string = "";
+  for (var i = 0; i < values.length; i += 1) {
+    const num = values[i].toString(16);
+    string += num.length == 1 ? "0" + num : num;
+  }
+  return string;
+}
+
+export default class App extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      text: []
+    };
+  }
+
+  componentDidMount() {
+    const manager = new BleManager();
+    manager.onStateChange(newState => {
+      if (newState != "PoweredOn") return;
+      this._log("Started scanning...");
+      manager.startDeviceScan(
+        null,
+        {
+          allowDuplicates: true
+        },
+        (error, device) => {
+          if (error) {
+            this._logError("SCAN", error);
+            return;
+          }
+          this._log("Device: " + device.name, device);
+        }
+      );
+    }, true);
+  }
+
+  _log = (text: string, ...args) => {
+    const message = "[" + Date.now() % 10000 + "] " + text;
+    this.setState({
+      text: [message, ...this.state.text]
+    });
+  };
+
+  _logError = (tag: string, error: BleError) => {
+    this._log(
+      tag +
+        "ERROR(" +
+        error.errorCode +
+        "): " +
+        error.message +
+        "\nREASON: " +
+        error.reason +
+        " (att: " +
+        error.attErrorCode +
+        ", ios: " +
+        error.iosErrorCode +
+        ", and: " +
+        error.androidErrorCode +
+        ")"
+    );
+  };
+
+  delay = () => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve();
+      }, 5000);
+    });
+  };
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <SafeAreaView
+        style={styles.container}
+      >
+        <Button
+          onPress={() => {
+            this.setState({
+              text: []
+            });
+          }}
+          title={"Clear"}
+        />
+        <FlatList
+          style={styles.container}
+          data={this.state.text}
+          renderItem={({ item }) => <Text> {item} </Text>}
+          keyExtractor={(item, index) => index.toString()}
+        />
+      </SafeAreaView>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    flex: 1
+  }
 });
---
+++ ios/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  sensortagexpo
+//
+//  Created by Przemysław Lenart on 25/09/2018.
+//  Copyright © 2018 650 Industries, Inc. All rights reserved.
+//
+
+import Foundation
